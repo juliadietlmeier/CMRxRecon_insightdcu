4d6d378ef61be2b7b9efb3fb2f1a9b0f39b8fb5d
juliadietlmeier <79544193+juliadietlmeier@users.noreply.github.com>
2023-07-27 15:51:18 +0100

    Add files via upload

---
+++ utils/coil_combine.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 27 15:46:11 2023
+
+@author: Julia Dietlmeier
+from CMRxRecon Github repo
+"""
+import numpy as np
+
+def rss(data: np.ndarray, dim: int = 0) -> np.ndarray:
+    """
+    Compute the Root Sum of Squares (RSS).
+
+    RSS is computed assuming that dim is the coil dimension.
+
+    Args:
+        data: The input array
+        dim: The dimensions along which to apply the RSS transform
+
+    Returns:
+        The RSS value.
+    """
+    return np.sqrt((data**2).sum(dim))
+
+
+def rss_complex(data: np.ndarray, dim: int = 0) -> np.ndarray:
+    """
+    Compute the Root Sum of Squares (RSS) for complex inputs.
+
+    RSS is computed assuming that dim is the coil dimension.
+
+    Args:
+        data: The input array
+        dim: The dimensions along which to apply the RSS transform
+
+    Returns:
+        The RSS value.
+    """
+    return np.sqrt(np.sum(np.abs(data)**2, axis=dim))
